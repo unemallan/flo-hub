f0c55f3e861226d41a11502363bef2ab7cdfd587
Umakaran Nemallan <unemallan@gmail.com>
2024-09-15 01:24:41 -0400

    update favicon

--- layouts/head.tsx
+++ layouts/head.tsx
@@ -15,7 +15,7 @@ export const Head = () => {
         content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         name="viewport"
       />
-      <link href="/favicon.ico" rel="icon" />
+      <link href="/favicon.ico?v=1.1" rel="icon" />
     </NextHead>
   );
 };
